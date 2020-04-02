93ab28615d3536a320649170399defe0d9842af7
xdsoar <xdsoar@gmail.com>
2020-04-02 14:05:07 +0800

    add json data and base rate function

--- src/service/HellCounting.spec.ts
+++ src/service/HellCounting.spec.ts
@@ -5,4 +5,14 @@ import { HellCounting }  from '@/service/HellCounting';
         expect(new HellCounting().farmingOnce().weaponName).toBe('hello');
     });
 
+    test('farming once should have a rate to get expect weapon', () => {
+        expect(new HellCounting().calculateSingleRate()).toBeCloseTo(1/200, 3);
+    });
+
+    test('should have much more chance to get weapon with more farming times', () => {
+        let hell = new HellCounting();
+        let onceTimeRate = hell.calculateSingleRate();
+        let tenTimeRate = hell.calculateRateWithSpecifiedTimes(10);
+        expect(tenTimeRate).toBeGreaterThan(onceTimeRate);
+    });
     
--- src/service/HellCounting.ts
+++ src/service/HellCounting.ts
@@ -3,6 +3,7 @@ import { Weapon } from '@/model/Weapon';
 export class HellCounting {
     weaponRepo: Array<Weapon>;
     weaponDict: Map<string, Weapon>;
+    static WEAPONLIST: Array<Weapon> = require("@/assets/data.json");
 
     constructor() {
         this.weaponRepo = new Array<Weapon>();
@@ -11,5 +12,14 @@ export class HellCounting {
 
     farmingOnce(): Weapon {
         return new Weapon("hello", "", "", 5);
+    }
+
+    calculateSingleRate(): number {
+        return 1/HellCounting.WEAPONLIST.length;
+    }
+
+    calculateRateWithSpecifiedTimes(farmingTimes: number): number {
+        const farmingOnce:number = this.calculateSingleRate()
+        return Math.pow(1-farmingOnce, farmingTimes);
     }
 }
