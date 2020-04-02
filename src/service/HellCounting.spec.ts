cd8d791f64df9888b0820564c19b7074e10858b7
xdsoar <xdsoar@gmail.com>
2020-04-02 17:57:45 +0800

    refactor to monte carlo method

--- src/service/HellCounting.spec.ts
+++ src/service/HellCounting.spec.ts
@@ -2,17 +2,29 @@ import { HellCounting }  from '@/service/HellCounting';
 
 
     test('should get a weapon successfully', () => {
-        expect(new HellCounting().farmingOnce().weaponName).toBe('hello');
-    });
-
-    test('farming once should have a rate to get expect weapon', () => {
-        expect(new HellCounting().calculateSingleRate()).toBeCloseTo(1/200, 3);
-    });
-
-    test('should have much more chance to get weapon with more farming times', () => {
         let hell = new HellCounting();
-        let onceTimeRate = hell.calculateSingleRate();
-        let tenTimeRate = hell.calculateRateWithSpecifiedTimes(10);
-        expect(tenTimeRate).toBeGreaterThan(onceTimeRate);
+        let count = 0;
+        for (let i = 0; i <1000; i++) {
+            if (hell.farmingOnce() != null) {
+                count++;
+            }
+        }
+        expect(count).toBeGreaterThanOrEqual(80);
+        expect(count).toBeLessThan(200);
     });
+
+    test('should graduate finally', () => {
+        let hell = new HellCounting();
+        let farmingTimes = hell.graduateOnce();
+        expect(farmingTimes).toBeGreaterThanOrEqual(100);
+    });
+
+    test('should gradute with specified weapon finally', () => {
+        let hell = new HellCounting();
+        let aim = HellCounting.WEAPONLIST[2];
+        let farmingTimes = hell.graduate([aim]);
+        expect(farmingTimes).toBeGreaterThanOrEqual(100);
+    })
+
+
     
--- src/service/HellCounting.ts
+++ src/service/HellCounting.ts
@@ -1,25 +1,68 @@
 import { Weapon } from '@/model/Weapon';
 
 export class HellCounting {
-    weaponRepo: Array<Weapon>;
-    weaponDict: Map<string, Weapon>;
     static WEAPONLIST: Array<Weapon> = require("@/assets/data.json");
 
-    constructor() {
-        this.weaponRepo = new Array<Weapon>();
-        this.weaponDict = new Map();
+
+    farmingOnce(): Weapon | null {
+        if (Math.random() > 0.9) {
+            let index = Math.floor(Math.random() * HellCounting.WEAPONLIST.length);
+            return HellCounting.WEAPONLIST[index];
+        }
+        return null;
     }
 
-    farmingOnce(): Weapon {
-        return new Weapon("hello", "", "", 5);
+
+    graduateOnce(): number {
+        let weaponRepo: Map<Weapon, number> = new Map();
+        for (let weapon of HellCounting.WEAPONLIST) {
+            weaponRepo.set(weapon, 0);
+        }
+
+        let farmingTimes = 0;
+        while (true) {
+            let weapon = this.farmingOnce();
+            if (weapon != null) {
+                let curCount = weaponRepo.get(weapon);
+                weaponRepo.set(weapon, curCount ? curCount + 1 : 1);
+                let aim = weaponRepo.get(HellCounting.WEAPONLIST[0]);
+                if (aim && aim > 4) {
+                    break;
+                }
+                if (farmingTimes > 10000) {
+                    break;
+                }
+            }
+            farmingTimes++;
+        }
+        return farmingTimes;
     }
 
-    calculateSingleRate(): number {
-        return 1/HellCounting.WEAPONLIST.length;
-    }
+    graduate(aimWeapons: Array<Weapon>): number {
+        let weaponRepo: Map<Weapon, number> = new Map();
+        for (let weapon of HellCounting.WEAPONLIST) {
+            weaponRepo.set(weapon, 0);
+        }
 
-    calculateRateWithSpecifiedTimes(farmingTimes: number): number {
-        const farmingOnce:number = this.calculateSingleRate()
-        return Math.pow(1-farmingOnce, farmingTimes);
+        let farmingTimes = 0;
+        while (true) {
+            let weapon = this.farmingOnce();
+            if (weapon != null) {
+                let curCount = weaponRepo.get(weapon);
+                weaponRepo.set(weapon, curCount ? curCount + 1 : 1);
+                let aimCount = 0;
+                for (let aim of aimWeapons) {
+                    let realCount = weaponRepo.get(aim);
+                    if (realCount && realCount >= 1){
+                        aimCount++;
+                    }
+                }
+                if (aimCount >= aimWeapons.length)
+                    break;
+            }
+            farmingTimes++;
+        }
+        console.log('times is ' + farmingTimes);
+        return farmingTimes;
     }
 }
