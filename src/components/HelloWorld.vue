6e225cd6eebbe21e0c19a38802c50b1828229c76
xdsoar <xdsoar@gmail.com>
2020-04-03 07:00:17 +0800

    WIP

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,68 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h1>{{ test()}}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p>For a guide and recipes on how to configure / customize this project</p>
+    <div id="weapons">
+      <el-form ref="form" >
+      <el-row :gutter="8" type="flex">
+        <el-col :span="2"> <el-form-item :span="6" label="活动名称" /> </el-col>
+        <el-col :span="2">
+          <el-select v-model="leftWeapon" placehodler="选择防具">
+            <el-option key="手搓" value="手搓" />
+            <el-option key="改恶" value="改恶" />
+            <el-option key="水果" value="水果" />
+          </el-select>
+        </el-col>
+        <el-col :span="4" class="hidden-md-and-down">
+          <el-input-number v-model="leftWeaponHold" :min="0" :max="5" label="已有件数" ></el-input-number>
+        </el-col>
+        <el-col :span="4">
+          <el-select v-model="headgear" placehodler="选择首饰">
+            <el-option key="大恍惚" value="大恍惚" />
+            <el-option key="幸运777" value="幸运777" />
+            <el-option key="上古术士" value="上古术士" />
+          </el-select>
+        </el-col>
+        <el-col :span="4" class="hidden-md-and-down">
+          <el-input-number v-model="headgearHold" :min="0" :max="3" label="已有件数" ></el-input-number>
+        </el-col>
+        <el-col :span="4" >
+          <el-select v-model="slot" placehodler="选择特殊装备">
+            <el-option key="军神" value="军神" />
+            <el-option key="灵宝" value="灵宝" />
+            <el-option key="时间战争" value="时间战争" />
+          </el-select>
+        </el-col>
+        <el-col :span="4" class="hidden-md-and-down">
+          <el-input-number v-model="slotHold" :min="0" :max="3" label="已有件数" ></el-input-number>
+        </el-col>
+      </el-row>
+      </el-form>
+      <span>
+        防具: {{ leftWeapon }}
+        <br />
+      </span>
+      <span>首饰: {{ headgear }}</span>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import { HellCounting } from '@/service/HellCounting';
+import { Component, Prop, Vue } from "vue-property-decorator";
+import 'element-ui/lib/theme-chalk/display.css';
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
-  
-
+  data() {
+    return {
+      leftWeapon: [],
+      headgear: [],
+      slot: [],
+      leftWeaponHold: 0,
+      headgearHold: 0,
+      slotHold: 0
+    };
+  }
 }
 </script>
 
@@ -58,4 +82,8 @@ li {
 a {
   color: #42b983;
 }
+
+.el-row {
+    margin-bottom: 20px;
+  }
 </style>
